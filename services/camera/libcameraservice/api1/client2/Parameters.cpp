4878ea95ec7ebddb996bd54b877ae370be4e6345
Shilin Victor <chrono.monochrome@gmail.com>
2015-10-09 07:43:35 +0300

    Revert "Camera API1: Ignore the video size change during recording"
    This reverts commit c572362427cdfda53142ad0b44818c269c1d9db1.

--- services/camera/libcameraservice/api1/client2/Parameters.cpp
+++ services/camera/libcameraservice/api1/client2/Parameters.cpp
@@ -1672,26 +1672,21 @@ status_t Parameters::set(const String8& paramString) {
     if (validatedParams.videoWidth != videoWidth ||
             validatedParams.videoHeight != videoHeight) {
         if (state == RECORD) {
-            ALOGW("%s: Video size cannot be updated (from %d x %d to %d x %d)"
-                    " when recording is active! Ignore the size update!",
-                    __FUNCTION__, videoWidth, videoHeight, validatedParams.videoWidth,
+            ALOGE("%s: Video size cannot be updated when recording is active!",
+                    __FUNCTION__);
+            return BAD_VALUE;
+        }
+        for (i = 0; i < availableVideoSizes.size(); i++) {
+            if ((availableVideoSizes[i].width ==
+                    validatedParams.videoWidth) &&
+                (availableVideoSizes[i].height ==
+                    validatedParams.videoHeight)) break;
+        }
+        if (i == availableVideoSizes.size()) {
+            ALOGE("%s: Requested video size %d x %d is not supported",
+                    __FUNCTION__, validatedParams.videoWidth,
                     validatedParams.videoHeight);
-            validatedParams.videoWidth = videoWidth;
-            validatedParams.videoHeight = videoHeight;
-            newParams.setVideoSize(videoWidth, videoHeight);
-        } else {
-            for (i = 0; i < availableVideoSizes.size(); i++) {
-                if ((availableVideoSizes[i].width ==
-                        validatedParams.videoWidth) &&
-                    (availableVideoSizes[i].height ==
-                        validatedParams.videoHeight)) break;
-            }
-            if (i == availableVideoSizes.size()) {
-                ALOGE("%s: Requested video size %d x %d is not supported",
-                        __FUNCTION__, validatedParams.videoWidth,
-                        validatedParams.videoHeight);
-                return BAD_VALUE;
-            }
+            return BAD_VALUE;
         }
     }
 
