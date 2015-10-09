bb75cf82a643115d3cf4dbaf96e373ba815aa51d
Shilin Victor <chrono.monochrome@gmail.com>
2015-10-09 08:58:11 +0300

    Revert "Use changed MediaCodec.PARAMETER_KEY_VIDEO_BITRATE value"
    This reverts commit 530fdbdc1b5491f3fbf172752834d1515701e142.

--- media/libstagefright/ACodec.cpp
+++ media/libstagefright/ACodec.cpp
@@ -4437,7 +4437,7 @@ bool ACodec::ExecutingState::onMessageReceived(const sp<AMessage> &msg) {
 
 status_t ACodec::setParameters(const sp<AMessage> &params) {
     int32_t videoBitrate;
-    if (params->findInt32("video-bitrate", &videoBitrate)) {
+    if (params->findInt32("videoBitrate", &videoBitrate)) {
         OMX_VIDEO_CONFIG_BITRATETYPE configParams;
         InitOMXParams(&configParams);
         configParams.nPortIndex = kPortIndexOutput;
