897b3761b92d9f7460039017e08978b0ff4db504
Shilin Victor <chrono.monochrome@gmail.com>
2015-10-10 21:29:26 +0300

    remove one more reference to timestampNs
    Change-Id: If7690e39873fd9216281c5e54dff425e4bbc1fa1

--- media/ndk/NdkMediaCodec.cpp
+++ media/ndk/NdkMediaCodec.cpp
@@ -346,9 +346,8 @@ media_status_t AMediaCodec_releaseOutputBuffer(AMediaCodec *mData, size_t idx, b
 
 EXPORT
 media_status_t AMediaCodec_releaseOutputBufferAtTime(
-        AMediaCodec *mData, size_t idx, int64_t timestampNs) {
-    ALOGV("render @ %" PRId64, timestampNs);
-    return translate_error(mData->mCodec->renderOutputBufferAndRelease(idx, timestampNs));
+        AMediaCodec *mData, size_t idx) {
+    return translate_error(mData->mCodec->renderOutputBufferAndRelease(idx));
 }
 
 //EXPORT
