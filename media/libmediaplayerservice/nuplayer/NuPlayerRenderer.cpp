9c92f6b089e9599a1355e459c08919830f881d49
Shilin Victor <chrono.monochrome@gmail.com>
2016-01-29 12:51:59 +0700

    Revert "Fix short sound playback"
    This reverts commit 15ce5f35187659ad757ee2ea6f70dcef5010c2c2.

--- media/libmediaplayerservice/nuplayer/NuPlayerRenderer.cpp
+++ media/libmediaplayerservice/nuplayer/NuPlayerRenderer.cpp
@@ -629,10 +629,6 @@ bool NuPlayer::Renderer::onDrainAudioQueue() {
 
             mAudioQueue.erase(mAudioQueue.begin());
             entry = NULL;
-            // Need to stop the track here, because that will play out the last
-            // little bit at the end of the file. Otherwise short files won't play.
-            mAudioSink->stop();
-            mNumFramesWritten = 0;
             return false;
         }
 
